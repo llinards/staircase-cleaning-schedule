4d4143cfd6eebebe935c72a715b56677397c64c6
LINARDS L <linards_lazdins@hotmail.com>
2022-07-26 00:51:43 +0300

    Updated port for Heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const CronJob = require("cron").CronJob;
 const Vonage = require("@vonage/server-sdk");
 const { createClient } = require("@supabase/supabase-js");
