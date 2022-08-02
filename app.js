b42c96f6e879c65d5235143be57bca1fa9781f34
LINARDS L <linards_lazdins@hotmail.com>
2022-08-02 20:22:31 +0300

    Updated for test CronJob.

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const getApartaments = async () => {
 
 const job = new CronJob(
 	// "*/1 * * * *",
-	"0 12 * * 2",
+	"0 5 * * 3",
 	() => {
 		const from = "Home";
 		const textApartamentFour = "Sveika, Liene! Ir pienākusi Tava kārta tīrīt kāpņu telpu!";
